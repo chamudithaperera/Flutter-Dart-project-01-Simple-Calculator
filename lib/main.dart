2b88bb0d1944073543b97560b6ecf6e3890fd530
Chamuditha Kavishan Perera <139614066+chamudithaperera@users.noreply.github.com>
2024-01-21 22:42:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,6 @@ import 'package:flutter/material.dart';
 void main()
 {
   runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());runApp(const MyApp());runApp(const MyApp());runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());
-  runApp(const MyApp());
-
-  runApp(const MyApp());
-
-  runApp(const MyApp());
-  runApp(const MyApp());
   
 }
 
